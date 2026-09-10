7069f7d340a02380550544649441266340244016
realaicasa <dynamicmike@gmail.com>
2026-09-10 14:51:08 -0500

    feat: add favicon, PWA icons, manifest, service worker

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+const CACHE = "fifeart-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["/", "/about"])));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
